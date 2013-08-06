b2bccf7cffa2b10a7d533d7114f5be9ad99ed448
jinwon <jinwon99@daum.net>
2013-08-06 21:56:12 +0900

    테스트 추가

---
+++ slipp-user/src/test/java/net/slipp/dao/bbs/MemoryBbsDaoTest.java
@@ -0,0 +1,40 @@
+package net.slipp.dao.bbs;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import net.slipp.domain.bbs.Bbs;
+
+import org.junit.Test;
+
+public class MemoryBbsDaoTest {
+
+	@Test
+	public void insert() throws SQLException {
+		
+		String bbsIdx = Long.toString(System.currentTimeMillis());
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Date now = new Date(System.currentTimeMillis());
+		
+		format.format(now);
+		String writeDate = format.format(now);	
+		
+		Bbs newbbs = new Bbs( bbsIdx, "user1", "1111", "subject", "content", writeDate);
+		
+		MemoryBbsDao bbsDao = new MemoryBbsDao();
+		bbsDao.insert(newbbs);
+		
+		//입력된 값이 존재하는지 체크함.
+		Bbs check_bbs = bbsDao.findBybbsIdx(newbbs.getBbsIdx());
+		
+		assertThat(check_bbs, is(newbbs));
+		
+	}
+}
